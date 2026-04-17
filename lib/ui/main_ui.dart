6f0eadd765b0951159c562b35fdace569549ecdb
andyyoung1219 <andyyoung1219@gmail.com>
2026-04-17 23:22:29 +0800

    入口

---
+++ lib/ui/main_ui.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'pages/chat_page.dart';
+import 'pages/collect_page.dart';
+
+class MainLayout extends StatefulWidget {
+  const MainLayout({super.key});
+
+  @override
+  State<MainLayout> createState() => _MainLayoutState();
+}
+
+class _MainLayoutState extends State<MainLayout> {
+  int _currentIndex = 0;
+
+  // 定義所有的子頁面
+  final List<Widget> _pages = const [
+    ChatPage(),
+    CollectPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: '聊天',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: '收藏',
+          ),
+        ],
+      ),
+    );
+  }
+}
